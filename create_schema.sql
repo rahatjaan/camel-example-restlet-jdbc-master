47b4e56a9d23b399db409986e0efc9a41c541f90
Rahat Ali <rahat.jaan@gmail.com>
2013-08-12 20:56:50 +0500

    More Changes

--- create_schema.sql
+++ create_schema.sql
@@ -18,3 +18,9 @@ CREATE  TABLE `camel`.`tenant` (
 
   PRIMARY KEY (`tenant_guid`) );
 
+
+
+INSERT INTO `camel`.`tenant` (`tenant_guid`, `name`, `phone_number`, `email`, `outbound_end_point_type`, `outbound_url`) VALUES ('test_guid', 'Jabs Solutions', '324234', 'rahat.jaan@gmail.com', '1', 'http://localhost:8081/pos/order');
+
+INSERT INTO `camel`.`tenant` (`tenant_guid`, `name`, `phone_number`, `email`, `outbound_end_point_type`, `outbound_url`) VALUES ('test_guid2', 'Elixir Technologies', '23323', 'rahat.jaan@gmail.com', '1', 'http://localhost:8081/pos/order');
+
