e2fb745566fc48f83bf7e6cfd3da4cbe54241fe6
Rahat Ali <rahat.jaan@gmail.com>
2013-08-15 17:07:14 +0500

    commit

--- src/main/java/ige/integration/utils/BeanToXML.java
+++ src/main/java/ige/integration/utils/BeanToXML.java
@@ -1,8 +1,5 @@
 package ige.integration.utils;
 
-import ige.integration.model.InRoomOrderPayLoad;
-import ige.integration.model.TenantInfo;
-
 import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
@@ -10,25 +7,24 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
 public class BeanToXML {
-public static String readObject(Object object) {
+	public static String readObject(Class classs,Object object) {
 		try {
-			JAXBContext jaxbContext = JAXBContext.newInstance(TenantInfo.class);/*
-			jaxbContext = JAXBContext.newInstance(object.getPayload().getClass());
-			jaxbContext = JAXBContext.newInstance(object.getObjectPayload().getClass());*/
+			JAXBContext jaxbContext = JAXBContext
+					.newInstance(classs);
 			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-	 
+
 			// output pretty printed
 			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-	 
+
 			StringWriter xml = new StringWriter();
-			//jaxbMarshaller.marshal(customer, file);
+			// jaxbMarshaller.marshal(customer, file);
 			jaxbMarshaller.marshal(object, xml);
-			//System.out.println(xml.toString());
+			// System.out.println(xml.toString());
 			return xml.toString();
-	 
-		      } catch (JAXBException e) {
-				e.printStackTrace();
-				return null;
-		      }
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
