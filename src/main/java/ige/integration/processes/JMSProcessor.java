4c5dea6a8e59afe995de591bd6e99c592193d1b7
Rahat Ali <rahat.jaan@gmail.com>
2013-08-15 19:50:32 +0500

    JMS END POINT

--- src/main/java/ige/integration/processes/JMSProcessor.java
+++ src/main/java/ige/integration/processes/JMSProcessor.java
@@ -1,12 +1,11 @@
 package ige.integration.processes;
 
-import ige.integration.model.TenantInfo;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
 public class JMSProcessor implements Processor{
 	public void process(Exchange exchange) throws Exception {
-		System.out.println("JMS ORDERS: "+exchange.getIn().getBody().toString());
+		System.out.println("JMS ORDERS: "+exchange.getIn().getBody());
+		exchange.getOut().setBody("Message is received at POS JMS Endpoint");
     }
 }
