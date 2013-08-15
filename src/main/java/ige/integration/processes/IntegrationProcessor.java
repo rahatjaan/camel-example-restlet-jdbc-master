ecfbff7272275c9b6e31a1f3854c47477acfd1ab
Rahat Ali <rahat.jaan@gmail.com>
2013-08-15 16:18:32 +0500

    Problem with response

--- src/main/java/ige/integration/processes/IntegrationProcessor.java
+++ src/main/java/ige/integration/processes/IntegrationProcessor.java
@@ -20,42 +20,14 @@ public class IntegrationProcessor implements Processor {
 	private DataSource dataSource;
 	
 	public void process(Exchange exchange) throws Exception {
-		// TODO Auto-generated method stub
 		InRoomOrderPayLoad payload = populateTenantInfo(exchange);
 //		System.out.println("IN PROCESSOR TOP: "+payload.toString());
 		//System.out.println("IN PROCESSOR: "+exchange.getIn(InRoomOrderPayLoad.class).getTenant().getTenantId());
-		String xml = BeanToXML.readObject(payload.getTenant());
-		exchange.getOut().setBody(xml);
-		exchange.getIn().setBody(xml);
-		System.out.println("PROCESSOR ENDS "+xml);
-//		if(payload.getTenant().getOutboundType()!=1){
-//			Response response = exchange.getIn().getHeader(RestletConstants.RESTLET_RESPONSE, Response.class);
-//			response.setStatus(Status.SUCCESS_OK);
-//			response.setEntity("<response>Order is sent!</response>", MediaType.TEXT_XML);
-//		}else
-//		{
-//			System.out.println("HTTP Outbound END POINT will serve this request");
-//		}
-        //exchange.getOut().setBody(response);
-		//System.out.println("EXCHANGE IS NOW!!! "+exchange.getIn().getBody().toString());
-		/*InRoomOrderPayLoad p = exchange.getIn().getBody(InRoomOrderPayLoad.class);
-		System.out.println("Object value: "+p.getPayload());*/
-		
-		/*System.out.println("Rahat hereeeeeeeeeeeeeee: "+exchange.getIn().getBody().toString());
-		String value = exchange.getIn().getBody().toString();
-		InRoomOrderPayLoad payload = new InRoomOrderPayLoad(value,exchange,new PopulateTenantInfo().getTenantInfo("test_guid2"));
+//		String xml = BeanToXML.readObject(InRoomOrderPayLoad.class,payload);
 		exchange.getOut().setBody(payload);
-		InRoomOrderPayLoad p = exchange.getIn().getBody(InRoomOrderPayLoad.class);
-		System.out.println("Object value: "+p.getPayload());*/
-		/*String body = exchange.getIn().getBody().toString();
-    	DataBean obj = XMLToBean.readXML(body);
-    	obj.setId(1);        	    	
-    	String xml = BeanToXML.readObject(obj);
-    	System.out.println("XML HERE IS: "+xml);
-    	exchange.getOut().setBody(xml);
-    	exchange.getOut().setHeaders(exchange.getIn().getHeaders());
-        System.out.println("Body is: " 
-                + exchange.getIn().getBody(String.class));*/
+//		exchange.getIn().setBody(payload.getPayload());
+		System.out.println("IntegrationProcessor \nout: "+payload.getPayload());
+
 	}
 	
 	
@@ -64,14 +36,6 @@ public class IntegrationProcessor implements Processor {
 		System.out.println("PopulateTenantInfo xml \n"+value);
 		InRoomOrderPayLoad payload = new InRoomOrderPayLoad(value,exchange,getTenantInfo("test_guid"));
 		System.out.println("PAYLOAD HERE IS: "+payload.getTenant().getTenantId());
-//		String val = "<outboundType>"+payload.getTenant().getOutboundType()+"</outboudType>"+"<outboundURL>"+payload.getTenant().getOutboundUrl()+"</outboundURL>";
-		
-		/*exchange.getOut().setBody(payload, InRoomOrderPayLoad.class);
-		System.out.println("MEDIUM IS: "+exchange.getIn().getBody().toString());*/
-		//exchange.getOut().setBody(payload,InRoomOrderPayLoad.class);
-		
-		/*InRoomOrderPayLoad obj = exchange.getIn(InRoomOrderPayLoad.class);
-		System.out.println("OBJECT HERE IS: "+obj.getTenant().getTenantId());*/
 		
 		return payload;
 	}
@@ -87,7 +51,7 @@ public class IntegrationProcessor implements Processor {
 						TenantInfo info = new TenantInfo();
 						if(result.next()){
 							info.setTenantId(result.getString("tenant_guid"));
-							info.setOutboundType(result.getInt("outbound_end_point_type"));
+							info.setOutboundType(result.getInt("outbound_end_point_type")+"");
 							info.setOutboundUrl(result.getString("outbound_url"));
 						}
 						else
